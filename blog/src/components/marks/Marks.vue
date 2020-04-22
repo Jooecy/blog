2430f937c644492de8739cf4ad9e749f0b9dca7e
Jooecy <657576276@qq.com>
2020-04-22 14:44:45 +0800

    确认删除优化

--- blog/src/components/marks/Marks.vue
+++ blog/src/components/marks/Marks.vue
@@ -67,7 +67,8 @@ export default {
         this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
           confirmButtonText: '确定',
           cancelButtonText: '取消',
-          type: 'warning'
+          type: 'warning',
+          center: true
         }).then(() => {
           this.$message({
             type: 'success',
