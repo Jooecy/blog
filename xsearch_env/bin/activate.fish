0cac584eb69e5ec84f912252b932ab5363085b77
Jooecy <657576276@qq.com>
2020-05-15 21:46:06 +0800

    新的环境测试

--- xsearch_env/bin/activate.fish
+++ xsearch_env/bin/activate.fish
@@ -52,7 +52,7 @@ if test -z "$VIRTUAL_ENV_DISABLE_PROMPT"
         set -l old_status $status
 
         # Prompt override?
-        if test -n "(xsearch_env) "            
+        if test -n "(xsearch_env) "
             printf "%s%s" "(xsearch_env) " (set_color normal)
         else
             # ...Otherwise, prepend env
